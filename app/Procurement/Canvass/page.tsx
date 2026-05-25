2dd92c00e0daf2bbf9557c4704cbb573ac74690f
Senn-9 <totanesjentzen@gmail.com>
2026-05-25 14:06:18 +0800

    t

--- app/Procurement/Canvass/page.tsx
+++ app/Procurement/Canvass/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+//this is canvass
+
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/utils/supabase/client";
